91b3ca39b684a1031f5ded85ed50bcfa472f9375
Athanasios Petsanis <athpets8@gmail.com>
2022-04-25 14:25:55 +0300

    Update

--- TextWorld/MyPy/Functions.py
+++ TextWorld/MyPy/Functions.py
@@ -1,7 +1,29 @@
 import gym
-import textworld
+import textworld.gym
 from textworld import EnvInfos
 
+# Register a text-based game as a new Gym's environment.
+
+
+# --- Make random game ---
+
+# options = textworld.GameOptions()
+# options.path = "./Random_Games/"
+# options.seeds = 432
+# game = make_game(options)
+# game_file = compile_game(game, options)
+# print(game_file)
+# -----------------------------
+
+
+# --- Make random game with quick command ---
+
+# THIS DOES NOT OVERWRITE GAMES WITH EXISTING NAMES!
+# !tw-make custom --world-size 2 --nb-objects 5 --theme house --quest-length 4 --quest-breadth 2 --entity-numbering --output "./Created_Games/MDP_Game.ulx" --seed 5 --format ulx
+
+# -------------------------------------
+
+# Load Game
 def load_game(game_file, max_steps=100):
 
     request_infos = EnvInfos(inventory=True, admissible_commands=True, entities=True, won=True, lost=True, intermediate_reward=True,
