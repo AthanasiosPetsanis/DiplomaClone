6cc80a638949e87d36a00cabe4d072f3702e519e
Athanasios Petsanis <athpets8@gmail.com>
2022-05-01 19:29:16 +0300

    Testing

--- MiniGrid/minigrid.py
+++ MiniGrid/minigrid.py
@@ -830,7 +830,10 @@ class MiniGridEnv(gym.Env):
         """
         Dense Rewarding. Each sub-goal rewards 1/number_of_goals for a total of 1 as before
         """
-        return (1 - 0.9 * (self.step_count / self.max_steps ))/ self.nof_goals
+        reward = (1 - 0.9 * (self.step_count / self.max_steps ))/ self.nof_goals
+
+        print(reward)
+        return reward 
 
     def _rand_int(self, low, high):
         """
