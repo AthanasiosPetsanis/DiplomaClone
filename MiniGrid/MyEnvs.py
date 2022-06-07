e4fa7809ea60bdae2049c375a9567bef1858ad29
Athanasios Petsanis <athpets8@gmail.com>
2022-06-07 21:18:19 +0300

    Update MyEnvs.py

--- MiniGrid/MyEnvs.py
+++ MiniGrid/MyEnvs.py
@@ -409,7 +409,7 @@ class Sparse_Env(MyMG_Env):
                 if (fwd_pos == [self.goal_width, self.goal_height]).all() \
                 and self.carrying.name == self.put_goals[1] and fwd_cell.name == self.put_goals[-1]:
                     done = True
-                    reward = 500
+                    reward = 10
 #                     reward = self._reward()
 
 
